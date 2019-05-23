814cac10c760608c1fe51095f650e4e2e65ccee0
EC2 Default User <ec2-user@ip-172-31-22-59.us-east-2.compute.internal>
2019-05-23 11:03:11 +0000

    host change heroku2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,8 +34,8 @@ config.action_mailer.raise_delivery_errors = true
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
+    :user_name      => ENV['app134207971@heroku.com'],
+    :password       => ENV['gfiq3niy9140'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
